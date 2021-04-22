167621949ff5502bf0b421ac474daa6a86fbbbf1
hj <ijshj10@gmail.com>
2021-04-22 20:06:12 +0900

    implemented dnd

--- src/components/circuit.js
+++ src/components/circuit.js
@@ -1,32 +1,123 @@
 import React from "react";
-
-const GATES = ["H", "X", "Y", "Z", 'M'];
+import {GATES, SIZE} from '../constants';
 
 class Composer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: null
+      clicked: null,
+      gates: [[], [], []],
+      dropI: null,
+      dropJ: null,
+      left: 0,
+      top: 0,
+      width: 0,
+      height: 0,
+      code: ["OPENQASM 2.0;\ninclude \"qelib1.inc\";\n\nqreg q[3];\ncreg q[3];\n"],
     };
   }
-  handleDrag = (event, kind) => {
+
+  getIndex = (clientX, clientY) => {
+    const x = clientX - this.state.left;
+    const y = clientY - this.state.top;
+    let i;
+    const rows = this.state.gates.length;
+    if (y < 0) {
+      return [undefined, undefined];
+    } else if(y < 65) { 
+      i = 0;
+    } else {
+      i = Math.min(Math.ceil((y - 65)/50), rows-1);
+    }
+
+    const cols = this.state.gates[i].length;
+
+    let j;
+    if (x < 0) {
+      return [undefined, undefined];
+    } else if(x < 46) {
+      j = 0;
+    } else {
+      j = Math.max(Math.min(Math.ceil((x-46)/45), cols), 0);
+    }
+    return [i,j];
+  }
+
+
+  handleDrag = (event, kind, i,j ) => {
+    if(i !== undefined) {
+      let qubit = this.state.gates[i].slice(0,j).concat(this.state.gates[i].slice(j+1));
+      let gates = this.state.gates.slice(0,i);
+      gates.push(qubit);
+      gates = gates.concat(this.state.gates.slice(i+1));
+      this.setState({gates});
+    }
+
     const clicked = {x: event.clientX, y: event.clientY, kind: kind};
     const mouseMove = (event) => {
       this.setState({clicked: {x: event.clientX, y: event.clientY, kind}});
     };
     const drop = (event) => {
       this.setState({clicked: null});
+      const [i,j] = this.getIndex(event.clientX, event.clientY);
+      const gates = this.state.gates.slice();
+
+      if(i !== undefined) { // do update gates
+        const newGates =  gates[i].slice(0, j);
+        newGates.push(kind);
+        gates[i] = newGates.concat(gates[i].slice(j));        
+      }
+      this.setState({gates, code: this.generateCode(gates)});
       window.removeEventListener("mousemove", mouseMove);
       window.removeEventListener("mouseup", drop);
     };
-    this.setState({clicked});
+    this.setState({clicked, dropI: i, dropJ: j});
     window.addEventListener("mousemove", mouseMove);
     window.addEventListener("mouseup", drop);
+  }
+
+  setDimension = (rect) => {
+    this.setState({
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height,
+    });
+  }
+
+  generateCode = (gates) => {
+    const qasm = ["OPENQASM 2.0;\ninclude \"qelib1.inc\";\n\nqreg q[3];\ncreg q[3];\n"];
+
+    let maxLen = 0;
+    gates.forEach(qubit => {
+      maxLen = Math.max(qubit.length, maxLen);
+    })
+
+    for(let j=0; j<maxLen; j++) {
+      gates.forEach((qubit, i) => {
+        if(qubit[j] !== undefined) {
+          let instr;
+          if(qubit[j] == 'M') {
+            instr = `measure q[${i}] -> c[${i}];`;
+          } else {
+            instr = `${qubit[j].toLowerCase()} q[${i}];`;
+          }
+          qasm.push(instr);
+        }
+      })
+    }
+
+
+    return qasm.join('\n');
+  }
+
+  runSimulation = () => {
+    
   }
   render() {
     return (
     <div className="flex flex-row">
-      <Sidebar />
+      <Sidebar runSimulation={this.runSimulation}/>
       <div className="flex flex-col space-y-0 flex-grow">
         <Menu />
         <Title />
@@ -39,9 +130,10 @@ class Composer extends React.Component {
             {this.state.clicked.kind}
           </div>
           }
-          <Circuit handleDrag={this.handleDrag} dragged={this.state.clicked}/>
+          <Circuit handleDrag={this.handleDrag} dragged={this.state.clicked} gates={this.state.gates} notifyDimension={this.setDimension}
+            width={this.state.width} getIndex={this.getIndex}/>
           </div>
-          <div className="w-2/6 ml-4" ><input className="w-full h-full" type="text" defaultValue="code"></input></div>
+          <div className="w-2/6 ml-4" ><textarea className="w-full h-full" value={this.state.code} readOnly></textarea></div>
           </div>
       </div>
     </div>);
@@ -51,92 +143,55 @@ class Composer extends React.Component {
 class Circuit extends React.Component {
   constructor(props) {
     super(props);
-    this.handleDrag = (event, kind, i, j) => {
-      let qubit = this.state.gates[i].slice(0,j).concat(this.state.gates[i].slice(j+1));
-      let gates = this.state.gates.slice(0,i);
-      gates.push(qubit);
-      gates = gates.concat(this.state.gates.slice(i+1));
-      this.setState({gates});
-      props.handleDrag(event, kind);
-    }
-
-
-    this.state = {
-      gates: [['H', 'X'], ['Y'], []],
-      top: 0,
-      left: 0,
-      width: 0,
-      height: 0,
-      dropHighlight: null,
-    }
+    this.handleDrag = props.handleDrag;
   }
 
   componentDidMount() {
     const rect = this.container.getBoundingClientRect();
-    
-    this.setState({
-        top: rect.top,
-        left: rect.left,
-        width: rect.width,
-        height: rect.height,
-        
-    });
+    this.props.notifyDimension(rect);
   }
-  getIndex = (clientX, clientY) => {
-    const x = clientX - this.state.left;
-    const y = clientY - this.state.top;
-    console.log(clientX, x,y);
-    let i;
-    const rows = this.state.gates.length;
-    if(y < 65) {
-      i = 0;
-    } else {
-      i = Math.min(Math.ceil((y - 65)/50), rows-1);
-    }
 
-    const cols = this.state.gates[i].length;
-
-    let j;
-    if(x < 46) {
-      j = 0;
-    } else {
-      j = Math.max(Math.min(Math.ceil((x-46)/45), cols), 0);
-    }
-    return [i,j];
-  }
 
 
   render() {
-    const width = this.state.width;
+    const width = this.props.width;
     const startX = Math.floor(width/20);
     const endX = startX * 19;
-    const SIZE = 32;
     const gates = [];
 
     let key = 0;
 
-    let gateArray = this.state.gates.slice();
+
+    let dropI, dropJ;
 
     if(this.props.dragged != null) {
-      let [i, j] = this.getIndex(this.props.dragged.x, this.props.dragged.y);
-      let dropHighlight = this.state.dropHighlight;
-      if(dropHighlight != null && dropHighlight.i == i && dropHighlight.j == j) {
-      } else {
-        dropHighlight = {i, j};
-        this.setState({dropHighlight});
-        gateArray[i].splice(j,0, null);
-      }
+      [dropI, dropJ] = this.props.getIndex(this.props.dragged.x, this.props.dragged.y);
     }
 
     
-    
-    gateArray.forEach((qubit, i) => {
-      qubit.forEach((gate, j) => {
+    let i = 0;
+    this.props.gates.forEach((qubit, ii) => {
+      let j =0
+      qubit.forEach((gate, jj) => {
+        if(i === dropI && j === dropJ) {
+          gates.push(<g key={key++}>
+          <rect x={30 + 45*j} y={40+50*i-16} width={SIZE} height={SIZE} fill="white"stroke="black" strokeDasharray="4 1" strokeWidth="1"/>
+          </g>);
+          j++;
+        }
         gates.push(<g key={key++}>
-          <rect onMouseDown={(event) => this.handleDrag(event, gate, i, j)} x={30 + 45*j} y={40+50*i-16} width={SIZE} height={SIZE} fill="white"stroke="black" strokeWidth="2"/>
-          <text onMouseDown={(event) => this.handleDrag(event, gate, i, j)} className="select-none" x={30+ 45*j +12} y={40+50*i+4} fontSize="12px" fontWeight="800">{gate}</text>
+          <rect onMouseDown={(event) => this.handleDrag(event, gate, ii, jj)} x={30 + 45*j} y={40+50*i-16} width={SIZE} height={SIZE} fill="white"stroke="black" strokeWidth="2"/>
+          <text onMouseDown={(event) => this.handleDrag(event, gate, ii, jj)} className="select-none" x={30+ 45*j +12} y={40+50*i+4} fontSize="12px" fontWeight="800">{gate}</text>
         </g>);
+        j++;
       });
+      if(i === dropI && j === dropJ) {
+        gates.push(<g key={key++}>
+        <rect x={30 + 45*j} y={40+50*i-16} width={SIZE} height={SIZE} fill="white"stroke="black" strokeDasharray="4 1" strokeWidth="1"/>
+        </g>);
+        j++;
+      }
+      i++;
     });
 
 
@@ -144,7 +199,7 @@ class Circuit extends React.Component {
 
     return(
     <svg className="w-full h-1/2 border-b-2 border-t-2" ref={el => (this.container = el)}>
-      {this.state.gates.map((qubit, i) => {
+      {this.props.gates.map((qubit, i) => {
         return (<line key={i} x1={20} x2={endX} y1={40+50*i} y2={40+50*i} strokeWidth={0.5} stroke="black"/>);
       })}
       {gates}
@@ -166,13 +221,13 @@ function GatePannel(props) {
 }
 
 
-function Sidebar() {
+function Sidebar(props) {
   return (
   <div className="flex flex-col space-y-4 w-12 border-r-2 h-screen">
   <button><span className="material-icons-outlined mt-4">folder</span></button>
   <button><span className="material-icons-outlined">file_download</span></button>
   <button><span className="material-icons-outlined">file_upload</span></button>
-  <button><span className="material-icons-outlined">play_arrow</span></button>
+  <button onClick={props.runSimulation}><span className="material-icons-outlined">play_arrow</span></button>
   </div>);
 }
 
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const GATES = ["H", "X", "Y", "Z", 'M'];
+export const SIZE = 32;
